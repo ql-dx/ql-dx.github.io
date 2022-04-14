dc76a170a4cf0c593289777067be4df60b8911ae
qldx <443445496@qq.com>
2022-04-14 19:45:35 +0800

    vue項目

---
+++ vue-project/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <my-home></my-home>
+  </div>
+</template>
+
+<script>
+import MyHome from './components/MyHome.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyHome,
+  }
+}
+</script>
+
+<style>
+#app {
+}
+</style>
---
+++ vue-project/src/components/MyHome.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>{{result}}</h1>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name:'MyHome',
+    data() {
+        return {
+            result:''
+        }
+    },
+    mounted() {
+        axios({
+            method:'post',
+            url:'https://api.apiopen.top/recommendPoetry'
+        }).then(
+            ({data:res}) => {
+                const {result} = res;
+                this.result = result;
+                console.log(res);
+            }
+        )
+    },
+}
+</script>
+
+<style>
+
+</style>
